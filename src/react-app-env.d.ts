fedcb19a558f282f20cba0227d5c50dadcb88b95
Nav <nav@navrao.me>
2021-10-10 14:41:05 +1300

    feat: handling svg imports

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+declare module '*.svg' {
+  /**
+   * Use `any` to avoid conflicts with
+   * `@svgr/webpack` plugin or
+   * `babel-plugin-inline-react-svg` plugin.
+   */
+  const content: any
+
+  export default content
+}
