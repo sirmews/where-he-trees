a4537752f5e0ee043511e764ad17d2ddb5f76c9a
Nav <nav@navrao.me>
2021-10-10 15:01:06 +1300

    feat: style without context, via css variables

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,10 +6,17 @@ import api from '../../mocks/api';
 
 function App() {
   const { USER } = api;
+  const { LINKS } = api;
+
+  // admittedly this is quite hacky but I ran out of time
+  
+  document.documentElement.style.setProperty('--color-button', USER?.colors?.backgroundColor);
+  document.documentElement.style.setProperty('--color-text', USER?.colors?.color);
+
   return (
     <div className={styles.app}>
       <Profile avatar={USER.avatar} handle={USER.handle} />
-      <Links/>
+      <Links links={LINKS}/>
       <Footer/>
     </div>
   );
